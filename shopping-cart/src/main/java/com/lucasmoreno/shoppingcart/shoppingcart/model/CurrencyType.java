56a41107327d93564df99a4c4e5d69203bfe8823
morenolf <l_f_moreno@hotmail.com>
2021-09-16 08:11:46 -0300

    Changing ORDINAL enum to STRING for jpa.

--- shopping-cart/src/main/java/com/lucasmoreno/shoppingcart/shoppingcart/model/CurrencyType.java
+++ shopping-cart/src/main/java/com/lucasmoreno/shoppingcart/shoppingcart/model/CurrencyType.java
@@ -2,15 +2,6 @@ package com.lucasmoreno.shoppingcart.shoppingcart.model;
 
 public enum CurrencyType {
 
-	USD(1), ARG(2);
-	
-	private int currency;
-	 
-	private CurrencyType(int currency) {
-		this.currency = currency;
-	}
- 
-	public int getProductStatus() {
-		return currency;
-	}
+	USD, ARG;
+
 }
