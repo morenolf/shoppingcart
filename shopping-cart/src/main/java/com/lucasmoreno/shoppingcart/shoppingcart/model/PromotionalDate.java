0f1100e85132afbc5a0acdf92f666de673ac356f
morenolf <l_f_moreno@hotmail.com>
2021-09-26 21:42:46 -0300

    Adding validations to Promotional dates and new method to set the date from api endpoint.

---
+++ shopping-cart/src/main/java/com/lucasmoreno/shoppingcart/shoppingcart/model/PromotionalDate.java
@@ -0,0 +1,5 @@
+package com.lucasmoreno.shoppingcart.shoppingcart.model;
+
+public class PromotionalDate {
+
+}
--- shopping-cart/src/main/java/com/lucasmoreno/shoppingcart/shoppingcart/service/PromotionalDateService.java
+++ shopping-cart/src/main/java/com/lucasmoreno/shoppingcart/shoppingcart/service/PromotionalDateService.java
@@ -5,6 +5,8 @@ import org.springframework.stereotype.Service;
 @Service
 public interface PromotionalDateService {
 
-	boolean validate();
+	void setPromotionalDate(boolean promotionalDate);
+
+	boolean validatePromotionalDate();
 
 }
